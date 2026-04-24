f0fc7a69e29582653292664987a9bfac2ed04c28
eucalypsih <eucalypsih>
2026-04-24 07:37:58 +0700

    init

---
+++ app/src/main/java/com/google/android/gms/example/appopenexample/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.google.android.gms.example.appopenexample
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.MobileAds
+
+/** The main activity in the app. */
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var googleMobileAdsConsentManager: GoogleMobileAdsConsentManager
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    setSupportActionBar(findViewById(R.id.toolBar))
+
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { view, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      view.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom)
+      insets
+    }
+
+    // Override the default implementation when the user presses the back key.
+    val onBackPressedCallback: OnBackPressedCallback =
+      object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+          moveTaskToBack(true)
+        }
+      }
+    onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+    googleMobileAdsConsentManager = GoogleMobileAdsConsentManager.getInstance(applicationContext)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.action_menu, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val menuItemView = findViewById<View>(item.itemId)
+    val activity = this
+    PopupMenu(this, menuItemView).apply {
+      menuInflater.inflate(R.menu.popup_menu, menu)
+      menu
+        .findItem(R.id.privacy_settings)
+        .setVisible(googleMobileAdsConsentManager.isPrivacyOptionsRequired)
+      show()
+      setOnMenuItemClickListener { popupMenuItem ->
+        when (popupMenuItem.itemId) {
+          R.id.privacy_settings -> {
+            // Handle changes to user consent.
+            googleMobileAdsConsentManager.showPrivacyOptionsForm(activity) { formError ->
+              if (formError != null) {
+                Toast.makeText(activity, formError.message, Toast.LENGTH_SHORT).show()
+              }
+            }
+            true
+          }
+          R.id.ad_inspector -> {
+            MobileAds.openAdInspector(activity) { error ->
+              // Error will be non-null if ad inspector closed due to an error.
+              error?.let { Toast.makeText(activity, it.message, Toast.LENGTH_SHORT).show() }
+            }
+            true
+          }
+          // Handle other branches here.
+          else -> false
+        }
+      }
+      return super.onOptionsItemSelected(item)
+    }
+  }
+}
